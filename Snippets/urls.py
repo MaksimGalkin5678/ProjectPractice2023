5c62617cec298170df729f293f4de195565955ed
student <galkin065@gmail.com>
2023-07-06 21:11:29 +0300

    First commit

---
+++ Snippets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from MainApp import views
+
+urlpatterns = [
+    path('', views.index_page),
+    path('add_snippet/', views.add_snippet_page, name='add_snippets'),
+    path('view_snippets/', views.snippets_page, name='view_snippets'),
+
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
